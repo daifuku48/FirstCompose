f0bf390734ffe7e7be626370a57b93a5634b4ca2
danil <natalya.haritonov.72@gmail.com>
2023-05-22 18:59:04 +0300

    fonts

---
+++ app/src/main/java/com/haritonovdanyluaa/firstcompose/SecondActivity.kt
@@ -0,0 +1,59 @@
+package com.haritonovdanyluaa.firstcompose
+
+import android.graphics.fonts.FontStyle
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.haritonovdanyluaa.firstcompose.ui.theme.FirstComposeTheme
+
+class SecondActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Box(modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+            contentAlignment = Alignment.Center)
+    {
+        Row(){
+            Text(text = "Fitness",
+                fontSize = 24.sp)
+            Text(text = "X",
+                fontSize = 40.sp,
+                )
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/haritonovdanyluaa/firstcompose/ui/theme/FontStyle.kt
@@ -0,0 +1,5 @@
+package com.haritonovdanyluaa.firstcompose.ui.theme
+
+import com.haritonovdanyluaa.firstcompose.R
+
+val poppins_Black = R.font.poppins_black
